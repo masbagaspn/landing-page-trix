ceb3181311c28816bb4c6bee7fe21a5170fe08fb
masbagaspn <masbagaspn@gmail.com>
2023-08-31 09:15:21 +0700

    remove unnecessary component

--- src/components/offers/OfferConvertion.jsx
+++
@@ -1,7 +0,0 @@
-import Offer from "./Offer";
-
-const OfferConvertion = () => {
-  return <Offer className="bg-grey flex flex-col gap-4">OfferConvertion</Offer>;
-};
-
-export default OfferConvertion;
--- src/components/offers/OfferOptimization.jsx
+++
@@ -1,9 +0,0 @@
-import Offer from "./Offer";
-
-const OfferOptimization = () => {
-  return (
-    <Offer className="offer-card bg-blue-primary">OfferOptimization</Offer>
-  );
-};
-
-export default OfferOptimization;
